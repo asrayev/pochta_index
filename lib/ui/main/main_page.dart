ba85f49fa23302b79a2c3c43eb02baf5aabffb95
Samandar <samandarahadjonov@gmail.com>
2022-12-18 21:13:12 +0500

    debugging

--- lib/ui/main/main_page.dart
+++ lib/ui/main/main_page.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:pochta_index/data/models/user_location_model.dart';
 import 'package:pochta_index/ui/main/pages/mails_page.dart';
 import 'package:pochta_index/ui/main/pages/saved_page.dart';
 import 'package:pochta_index/utils/my_colors.dart';
 import 'package:pochta_index/utils/my_icons.dart';
-
-import '../../utils/media_query.dart';
-
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
@@ -18,77 +13,33 @@ class MainPage extends StatefulWidget {
 }
 int currentPage=0;
 List pages=[
- MailsPage(),
+  MailsPage(),
   SavedPage()
 ];
-
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-        body: Stack(
-          children: [
-            pages[currentPage],
-            Column(
-              children: [
-                SizedBox(height: m_h(context)*0.92,),
-                Center(
-                  child: Container(
-                    height: m_h(context)*0.08,
-                    decoration: BoxDecoration(
+     body: pages[currentPage],
+     bottomNavigationBar: BottomNavigationBar(
+       currentIndex: currentPage,
+       onTap: (v){
+         setState(() {currentPage=v;});
+       },
+       backgroundColor: MyColors.C_0F1620,
+       unselectedItemColor: MyColors.C_938F88,
+       selectedItemColor: MyColors.C_46AEF5,
+       items: [
+         BottomNavigationBarItem(icon: SvgPicture.asset(MyIcons.location, height: 30.h, width: 30.w, color:  colorGenerator(0)),label: "Mails"),
+         BottomNavigationBarItem(icon: SvgPicture.asset(MyIcons.pin, height: 23.h, width: 23.w,color:  colorGenerator(1)),label: "Saved"),
 
-                      color: MyColors.C_0F1620,
-
-
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.only(right: m_w(context)*0.23, left: m_w(context)*0.25),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          InkWell(
-                              onTap: ((){
-                                setState(() {
-                                  currentPage=0;
-                                });
-                              }),
-                              child: Padding(
-                                padding: EdgeInsets.only(top: m_h(context)*0.01),
-                                child: Column(
-                                  children: [
-                                    SvgPicture.asset(MyIcons.location, height: 30.h, width: 30.w, color:  currentPage==0? MyColors.C_46AEF5 : MyColors.C_938F88),
-                                    Text("Mails", style: GoogleFonts.mulish(color:  currentPage==0? MyColors.C_46AEF5 : MyColors.C_938F88, fontWeight: FontWeight.w600),)
-                                  ],
-                                ),
-                              )),
-                          InkWell(
-                              onTap: ((){
-                                setState(() {
-                                  currentPage=1;
-                                });
-                              }),
-                              child: Padding(
-                                padding: EdgeInsets.only(top: m_h(context)*0.016),
-                                child: Column(
-                                  children: [
-                                    SvgPicture.asset(MyIcons.pin, height: 23.h, width: 23.w,color:  currentPage==1? MyColors.C_46AEF5 : MyColors.C_938F88),
-                                    SizedBox(height: m_h(context)*0.005,),
-                                    Text("Saved", style: GoogleFonts.mulish(color:  currentPage==1? MyColors.C_46AEF5 : MyColors.C_938F88, fontWeight: FontWeight.w600),)
-                                  ],
-                                ),
-                              )),
-
-
-
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
-        )
+       ],
+     ),
     );
   }
+  Color colorGenerator(int number){
+    return currentPage==number? MyColors.C_46AEF5 : MyColors.C_938F88;
+  }
 }
+
+
